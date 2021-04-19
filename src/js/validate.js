b993421d8d60baca9be3145f781d1e36805845e6
mustafa <mmalik57@myseneca.ca>
2021-04-19 02:17:39 -0400

    first commit

---
+++ src/js/validate.js
@@ -0,0 +1,46 @@
+function validate(event) {
+  // TODO - write custom validation logic to validate the longitude and latitude
+  // values. The latitude value must be a number between -90 and 90; the
+  // longitude value must be a number between -180 and 180. If either/both are
+  // invalid, show the appropriate error message in the form, and stop the
+  // form from being submitted. If both values are valid, allow the form to be
+  // submitted.
+  var latitude = document.querySelector("#latitude").value;
+  var longitude = document.querySelector("#longitude").value;
+  var latitude_isValid = false;
+  var longitude_isValid = false;
+
+  if (
+    !isNaN(latitude) &&
+    parseInt(latitude) >= -90 &&
+    parseInt(latitude) <= 90
+  ) {
+    document.querySelector("#Latitude-Checker").style.display = "none";
+    latitude_isValid = true;
+  } else {
+    document.querySelector("#Latitude-Checker").style.display = "inline-block";
+  }
+
+  if (
+    !isNaN(longitude) &&
+    parseInt(longitude) >= -180 &&
+    parseInt(longitude) <= 180
+  ) {
+    document.querySelector("#Longitude-Checker").style.display = "none";
+    longitude_isValid = true;
+  } else {
+    document.querySelector("#Longitude-Checker").style.display = "inline-block";
+  }
+
+  if (!latitude_isValid || !longitude_isValid) {
+    event.preventDefault(); //should stop latitude and longitude to submit to formspree if data before is not valid
+  }
+
+  //console.log('TODO - validate the longitude, latitude values before submitting');
+}
+
+// Wait for the window to load, then set up the submit event handler for the form.
+window.onload = function () {
+  const form = document.querySelector("form");
+  form.onsubmit = validate;
+};
